504159f82a0ed727eb2ba133df6b14c1042b274e
orel katz <orel.katz@sisense.com>
2017-02-01 17:33:44 +0200

    commit with aorking version. start of url link

--- paletteGen.js
+++ paletteGen.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ng-file-model']);
 
 var $dom = prism.$injector.get('ux-controls.services.$dom');
-var $widnowStack = prism.$injector.get('ux-controls.services.$windowStack');
+var $windowStack = prism.$injector.get('ux-controls.services.$windowStack');
 
 
 var paletteButton = {
@@ -11,46 +11,60 @@ var paletteButton = {
   execute: function () {
     $dom.modal({
       scope:{
-        postPalette: function(name) {
+        postPalette: function(name,imgUrl) {
+          console.log(imgUrl);
           var colorThief = new ColorThief();
           var $http = prism.$injector.get('$http');
           var image = document.getElementById('paletteFile').files[0];
-          if (image === null || image === undefined || name === undefined) {
-            if (name === undefined || name === null) {
+          if (!name || (!imgUrl && !image)) {
+            if (!name ) {
               document.getElementById('noName').className = 'noImage';
               setTimeout(function(){
                 document.getElementById('noName').className = 'invisible';},2000);
-          } else if (image === null || image === undefined) {
+          } else {
             document.getElementById('noImageError').className = 'noImage';
             setTimeout(function(){
               document.getElementById('noImageError').className = 'invisible';},2000);
           }
 
           } else {
-            var reader  = new FileReader();
-            reader.onload = function(e) {
+            var imgCycle = function (src) {
               var img = document.createElement('img');
-              img.src = e.target.result;
-              var colorsArray = colorThief.getPalette(img,8);
-              var hexColors = colorsArray.map(function(x){
-                var newColor = new Color(x[0],x[1],x[2]);
-                return newColor.toHex();});
-              var paletteObj = {'name': name, 'colors': hexColors,  'isDefault': true, 'sortOrder': 0};
+              img.onload = function() {
+                var colorsArray = colorThief.getPalette(img,8);
+                var hexColors = colorsArray.map(function(x){
+                  var newColor = new Color(x[0],x[1],x[2]);
+                  return newColor.toHex();});
+                var paletteObj = {'name': name, 'colors': hexColors,  'isDefault': true, 'sortOrder': 0};
 
-              $http({
-                method: 'POST',
-                url: 'http://localhost:8081/api/palettes',
-                data: paletteObj
-              }).then(function successCallback(response) {
-                return response;
-              }, function errorCallback(response) {
-                return response;
-              });
+                $http({
+                  method: 'POST',
+                  url: 'http://localhost:8081/api/palettes',
+                  data: paletteObj
+                }).then(function successCallback(response) {
+                  return response;
+                }, function errorCallback(response) {
+                  return response;
+                });
 
-              $widnowStack.closeAllWindowsAbove();
+                $windowStack.closeAllWindowsAbove();
+
+              };
+              img.src = src;
 
             };
+            if (imgUrl) {
+              imgCycle(imgUrl);
+            } else {
+
+            var reader  = new FileReader();
+            reader.onload = function(e) {
+              imgCycle(e.target.result);
+
+            };
+
             reader.readAsDataURL(image);
+          }
 
           }
 
