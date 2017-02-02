db328472ae30584a6726220ce77412255982c12f
orel katz <orel.katz@sisense.com>
2017-02-02 18:19:04 +0200

    ready to go with the palette!!!

--- paletteGen.js
+++ paletteGen.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ng-file-model']);
 
 var $dom = prism.$injector.get('ux-controls.services.$dom');
-var $windowStack = prism.$injector.get('ux-controls.services.$windowStack');
+
 
 
 var paletteButton = {
@@ -11,8 +11,58 @@ var paletteButton = {
   execute: function () {
     $dom.modal({
       scope:{
+
+        imgWatcher: function(filush,imgUrl) {
+          // console.log(filush);
+          // console.log(imgUrl);
+          console.log('change!!');
+          var image = document.getElementById('paletteFile').files[0];
+          var img = document.createElement('img');
+          var colorThief = new ColorThief();
+          var imgCycleW = function (src) {
+            img.onload = function() {
+              var colorsArray = colorThief.getPalette(img,8);
+
+              var hexColors = colorsArray.map(function(x){
+                var newColor = new Color(x[0],x[1],x[2]);
+
+                return newColor.toHex();});
+
+              hexColors.forEach(function(hex) {
+                console.log('color');
+                var bla = document.getElementById('colorsPreview');
+                var viewHex = document.createElement('div');
+                // hex.style.backgroundColor = hex;
+                bla.appendChild(viewHex);
+                viewHex.style.backgroundColor = hex;
+                viewHex.style.width = '30px';
+                viewHex.style.height = '30px';
+                viewHex.style.display = 'inline-block';
+                viewHex.style.border = '1px solid white';
+
+
+              });
+            };
+            img.src = src;
+
+          };
+
+
+          if (!imgUrl) {
+            var reader  = new FileReader();
+            reader.onload = function(e) {
+              imgCycleW(e.target.result);
+            };
+
+              reader.readAsDataURL(image);
+          } else {
+            imgCycleW(imgUrl);
+          }
+
+
+        },
         postPalette: function(name,imgUrl) {
-          
+
           var colorThief = new ColorThief();
           var $http = prism.$injector.get('$http');
           var image = document.getElementById('paletteFile').files[0];
@@ -21,11 +71,11 @@ var paletteButton = {
               document.getElementById('noName').className = 'noImage';
               setTimeout(function(){
                 document.getElementById('noName').className = 'invisible';},2000);
-          } else {
+            } else {
             document.getElementById('noImageError').className = 'noImage';
             setTimeout(function(){
               document.getElementById('noImageError').className = 'invisible';},2000);
-          }
+            }
 
           } else {
             var imgCycle = function (src) {
@@ -35,12 +85,19 @@ var paletteButton = {
                 var colorsArray = colorThief.getPalette(img,8);
 
                 var hexColors = colorsArray.map(function(x){
-
                   var newColor = new Color(x[0],x[1],x[2]);
 
                   return newColor.toHex();});
 
+                // hexColors.forEach(function(hex) {
+                //   console.log("color");
+                //   var viewHex = document.createElement(hex);
+                //   // hex.style.backgroundColor = hex;
+                //   document.body.appendChild(viewHex);
+                //   hex.style.backgroundColor = hex;
+                // });
                 var paletteObj = {'name': name, 'colors': hexColors,  'isDefault': true, 'sortOrder': 0};
+
 
                 $http({
                   method: 'POST',
@@ -88,7 +145,8 @@ var paletteButton = {
 
           }
 
-        }
+          },
+
       },
       templateUrl: '/plugins/paletteOfficial/popupHtml.html',
     });
